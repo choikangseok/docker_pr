12dfad678be5adc79d80d016703e7d75a1fb3f45
choikangseok <ktos5427@naver.com>
2020-06-25 10:54:01 +0900

    test2 + html

---
+++ test2/do.sh
@@ -0,0 +1,4 @@
+for i in $(seq 9); do
+  let ans=4*$i
+  echo 4 X $i = $ans
+done
---
+++ test2/mkdir100_do.sh
@@ -0,0 +1,13 @@
+for i in $(seq 1 100); do
+  if [ $i -eq 3 ]; then
+    echo $i
+  fi
+done
+  # mkdir $i
+  # docker run -it --name uu$i ubuntu # 100개  컨테이너 생성
+
+#gt
+#ge
+
+#ne
+#eq
